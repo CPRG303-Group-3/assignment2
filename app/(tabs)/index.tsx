26665886137f0792ce46d66b50ec80bff7ac0b54
Harsimar Sahota <harsimar1288@gmail.com>
2025-06-17 17:36:08 -0600

    Images added for each city

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import { useRouter } from "expo-router";
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, Image } from "react-native";
 export default function WelcomeScreen() {
   const router = useRouter();
   return (
     <View>
       <Text style={styles.welcome}>Welcome to My New App</Text>
+      <Image 
+        source={{uri: "https://plus.unsplash.com/premium_photo-1673241100156-2e04fca1a4af?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FsZ2FyeXxlbnwwfHwwfHx8MA%3D%3D"}}
+        accessibilityLabel="City of Calgary"
+      />
       <Pressable
         onPress={() => router.push("https://www.calgary.ca/home.html")}
       >
--- app/(tabs)/welcome.tsx
+++ app/(tabs)/welcome.tsx
@@ -1,10 +1,13 @@
 import { useRouter } from "expo-router";
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, Image } from "react-native";
 
 export default function WelcomePage() {
   const router = useRouter();
   return (
     <View>
+      <Image 
+        source={{uri: "https://images.unsplash.com/photo-1574541647051-099cedfb7f8f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZWRtb250b258ZW58MHx8MHx8fDA%3D"}}
+        accessibilityLabel= "City of Edmonton"/>
       <Pressable onPress={() => router.push("https://www.edmonton.ca/")}>
         <Text style={styles.city}>Go to city page</Text>
       </Pressable>
