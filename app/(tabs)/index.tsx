50ea4a250488ffc59430a2227ae69ca01121b2e5
Chioma <96903876+chiomaeme@users.noreply.github.com>
2025-06-17 17:25:53 -0600

    Tab navigation setup for the cities

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,1 +1,26 @@
-export default function WelcomeScreen() {}
+import { useRouter } from "expo-router";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+export default function WelcomeScreen() {
+  const router = useRouter();
+  return (
+    <View>
+      <Text style={styles.welcome}>Welcome to My New App</Text>
+      <Pressable
+        onPress={() => router.push("https://www.calgary.ca/home.html")}
+      >
+        <Text style={styles.city}>Go to city page</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  welcome: {
+    textAlign: "center",
+    fontSize: 24,
+  },
+  city: {
+    textAlign: "center",
+    fontSize: 16,
+  },
+});
--- app/(tabs)/welcome.tsx
+++ app/(tabs)/welcome.tsx
@@ -1,3 +1,20 @@
-import React from "react";
+import { useRouter } from "expo-router";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
-export default function WelcomePage() {}
+export default function WelcomePage() {
+  const router = useRouter();
+  return (
+    <View>
+      <Pressable onPress={() => router.push("https://www.edmonton.ca/")}>
+        <Text style={styles.city}>Go to city page</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  city: {
+    textAlign: "center",
+    fontSize: 16,
+  },
+});
